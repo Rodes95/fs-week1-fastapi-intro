db2550cf474df6e3664234afd9e756a82d5b81a9
Rodrigo Escariz <rodrigoescariz@gmail.com>
2026-02-01 21:28:15 +0100

    refactor: extract CORS origins configuration

--- main.py
+++ main.py
@@ -5,12 +5,14 @@ from typing import Optional
 
 app = FastAPI()
 
+origins = [
+     "http://127.0.0.1:5173", # Vite
+     "http://localhost:5173",
+]
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://127.0.0.1:5173",
-        "http://localhost:5173",
-    ],
+    allow_origins= origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
